454c334abb2e7959d060fbceeae4286311894780
LuHenriSouza <luizhapsouza@gmail.com>
2025-02-04 12:51:06 -0300

    fix bonus

--- src/bonus/main_bonus.c
+++ src/bonus/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: lhenriqu <lhenriqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 13:22:25 by lhenriqu          #+#    #+#             */
-/*   Updated: 2025/02/04 12:22:53 by lhenriqu         ###   ########.fr       */
+/*   Updated: 2025/02/04 12:47:58 by lhenriqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ static void	exec_cmds(t_pipex *pipex)
 		pipex->pids[i] = fork();
 		if (pipex->pids[i] == PID_CHILD)
 			exec_process(pipex->cmds[i]);
-		
+		i++;
 	}
 }
 
@@ -32,7 +32,7 @@ int	main(int argc, char *argv[], char *envp[])
 	int		i;
 	
 	pipex = get_pipex();
-	if (argc != 5)
+	if (argc < 5)
 	{
 		print_error("Expected: ./pipex <file_in> <cmd1> ... <cmd2> <file_out>");
 		return (1);
